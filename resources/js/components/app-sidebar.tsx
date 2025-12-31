eb9686b2c706dbd9c4c1be21c559decc38b2c4b6
DouradoCtrl <gdouradoestudante@gmail.com>
2025-12-31 00:40:05 -0300

    add: clientes, processos, tribunais a sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,7 +13,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, Users, Gavel, FileText } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -21,6 +21,21 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: dashboard(),
         icon: LayoutGrid,
+    },
+    {
+        title: 'Clientes',
+        href: dashboard(),
+        icon: Users,
+    },
+    {
+        title: 'Processos',
+        href: dashboard(),
+        icon: FileText,
+    },
+    {
+        title: 'Tribunais',
+        href: dashboard(),
+        icon: Gavel,
     },
 ];
 

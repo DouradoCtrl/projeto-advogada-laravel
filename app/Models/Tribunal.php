d5315186fedbc0d84d971949b39c85154e036791
DouradoCtrl <gdouradoestudante@gmail.com>
2025-12-31 11:36:28 -0300

    add: create model 'tribunal'

---
+++ app/Models/Tribunal.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Tribunal extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tribunais';
+    protected $fillable = [
+        'nome',
+        'sigla',
+        'api_endpoint',
+    ];
+}

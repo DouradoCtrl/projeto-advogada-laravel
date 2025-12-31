fb93d6f7eef84bcfd733bb8008df08adbee4c55d
DouradoCtrl <gdouradoestudante@gmail.com>
2025-12-31 01:32:51 -0300

    add: return token in view 'cnjToken'

--- app/Http/Controllers/CnjApiController.php
+++ app/Http/Controllers/CnjApiController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\CnjApiToken;
+use Inertia\Inertia;
 
 class CnjApiController extends Controller
 {
-    //
+    public function index()
+    {
+        $tokens = CnjApiToken::all();
+        return Inertia::render('cnjToken', [
+            'tokens' => $tokens,
+        ]);
+    }
+
+    public function getToken(Request $request)
+    {
+        // Lógica para obter o token do CNJ
+    }
 }

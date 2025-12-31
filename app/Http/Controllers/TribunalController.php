b160498ef5516e7629b8707373631ba6fe513743
DouradoCtrl <gdouradoestudante@gmail.com>
2025-12-31 11:39:01 -0300

    add: create method index (read) 'tribunais'

--- app/Http/Controllers/TribunalController.php
+++ app/Http/Controllers/TribunalController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tribunal;
+use Inertia\Inertia;
 
 class TribunalController extends Controller
 {
-    //
+    public function index()
+    {
+        $tribunais = Tribunal::paginate(15);
+        return Inertia::render('tribunais', [
+            'tribunais' => $tribunais,
+        ]);
+    }
 }
